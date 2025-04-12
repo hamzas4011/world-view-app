ba051e9947a80ecb63e6427abd1d6f429f3a43c5
Hamza Simsek <hamzas4011@hotmail.com>
2025-04-12 13:17:26 +0200

    code for the backend

---
+++ src/app/api/countries/route.ts
@@ -0,0 +1,18 @@
+
+
+export async function GET() {
+    try {
+      const res = await fetch('https://restcountries.com/v3.1/all')
+  
+      if (!res.ok) {
+        return new Response('Failed to fetch countries', { status: 500 })
+      }
+  
+      const countries = await res.json()
+      return Response.json(countries) 
+    } catch (error) {
+      console.error('API error:', error)
+      return new Response('Server error', { status: 500 })
+    }
+  }
+  
